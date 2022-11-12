29c98f1d92f7f0a5c07276601dc2d61b19dde12c
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-12 08:23:20 +0000

    dns module which is used to get ip add of the current server

---
+++ nodejs_dns/dns_getservers1.js
@@ -0,0 +1,7 @@
+// Node.js program to demonstrate the dns.resolveSoa() method
+   
+// Accessing dns module
+const dns = require('dns');
+     
+// Reading IP address of the current host and printing it to the console
+console.log(dns.getServers());
