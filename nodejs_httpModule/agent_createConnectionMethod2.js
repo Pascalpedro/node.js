0f4133ebebc10e040d2733ac6ef8b182e8be8d91
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-17 21:55:16 +0000

    an inbuilt application programming interface of the ‘Http‘ module which is used to produce socket or stream

---
+++ nodejs_httpModule/agent_createConnectionMethod2.js
@@ -0,0 +1,28 @@
+// Node.js program to demonstrate the agent.createConnection() method 
+
+// Importing http module
+const http = require('http');
+var agent = new http.Agent({});
+
+// Creating new agent
+const aliveAgent = new http.Agent({
+    keepAlive: true,
+    maxSockets: 0, maxSockets: 5,
+});
+
+// Creating new agent
+var agent = new http.Agent({});
+
+// Creating new connection
+var createConnection = aliveAgent.createConnection;
+
+// Creating new connection
+var createConnection = agent.createConnection;
+console.log('Connection successfully created...');
+
+// Printing the connection
+console.log(createConnection);
+console.log('Connection successfully created...');
+
+// Printing the connection
+console.log('Connection: ', createConnection);
