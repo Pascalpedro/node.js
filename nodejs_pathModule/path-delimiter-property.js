bdc8ef277a231542b3eadb5ecc6fcb8e9b0aa58e
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-19 21:46:34 +0000

    an inbuilt app prog interface of the path module which is used to get platform-specific path delimiter

---
+++ nodejs_pathModule/path-delimiter-property.js
@@ -0,0 +1,7 @@
+// Node.js program to demonstrate the path.delimiter property
+
+// Allocating path module
+const path = require('path');
+
+// Printing path.delimiter value
+console.log(path.delimiter);
