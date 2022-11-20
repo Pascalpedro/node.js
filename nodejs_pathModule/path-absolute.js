e96cf15228a5dd43277ca04d5fdc409b19009056
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-20 22:03:06 +0000

    method is used to check whether the given path is an absolute path or not

---
+++ nodejs_pathModule/path-absolute.js
@@ -0,0 +1,16 @@
+// Node.js program to demonstrate the path.isAbsolute() method
+
+// Import the path module
+const path = require('path');
+
+path1 = path.isAbsolute("/user/bash/");
+console.log(path1);
+
+path2 = path.isAbsolute("user/bash/readme.md");
+console.log(path2);
+
+path3 = path.isAbsolute("/user/bash/readme.md");
+console.log(path3);
+
+path4 = path.isAbsolute("..");
+console.log(path4);
