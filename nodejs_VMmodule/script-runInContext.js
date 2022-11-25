d9e96c5514f0a89e6eb5c084c78e189ee453ef27
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-25 21:17:00 +0000

    method is used to run the compiled code which is present inside the vm.

---
+++ nodejs_VMmodule/script-runInContext.js
@@ -0,0 +1,21 @@
+t util = require('util');
+const vm = require('vm');
+
+// Constructing context
+const contextobj = {
+  name: 'Nidhi',
+  articles: 60
+};
+
+// Constructing script
+const script = new vm.Script('articles *= 10;');
+
+// Contextifying object
+ vm.createContext(contextobj);
+
+// Calling runInContext method
+script.runInContext(contextobj):
+
+// Displays output
+console.log(contextobj);
+			  
