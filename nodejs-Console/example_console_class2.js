456791c3f75a4698e8c083789c36043876eda6a7
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-28 21:19:28 +0000

    Node.js console module is a global object that provides a simple debugging console similar to JavaScript to display different levels of message

---
+++ nodejs-Console/example_console_class2.js
@@ -0,0 +1,13 @@
+// It will display 'This is the first object example' to stdout
+console.log('This is the first object example');
+
+// It will display 'This is the second object example' to stdout
+console.log('This is the %s example', 'second object');
+
+// It will display 'Error: New Error has happened' to stderr
+console.error(new Error('New Error has happened'));
+
+const obj = 'third object';
+
+// It will display 'This is the third object example' to stderr
+console.warn(`This is the ${obj} example`);
