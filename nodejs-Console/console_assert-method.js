538e91d3fc3fc3576ae0efd254687463f33ef767
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-29 20:27:07 +0000

    an inbuilt app prog interface of the console module which is used to assert value passed to it as a parameter, i.e. it checks whether the value is true or not and print an error message, if provided and failed to assert the value

---
+++ nodejs-Console/console_assert-method.js
@@ -0,0 +1,8 @@
+// Node.js program to demonstrate the console.assert() Method
+
+// Accessing console module
+const console = require('console');
+
+// Calling console.assert() method
+console.assert(true, "error message 1");
+console.assert(false, "error message 2");
