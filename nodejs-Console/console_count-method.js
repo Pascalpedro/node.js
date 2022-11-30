97caa127f6bfba9750c3994255dd07cf13f84c20
pascalpedro <Attamapascalpedro@gmail.com>
2022-11-30 21:20:52 +0000

    an inbuilt application programming interface of the console module which is used to count label passed to it as a parameter, by maintaining an internal counter for that specific label.

---
+++ nodejs-Console/console_count-method.js
@@ -0,0 +1,14 @@
+// Node.js program to demonstrate the console.count() Method
+
+// Accessing console module
+const console = require('console');
+
+// Calling console.count() 
+console.count("a");
+console.count("b");
+console.count("a");
+console.count("a");
+console.count("a");
+console.count("b");
+console.count("b");
+console.count("b")
